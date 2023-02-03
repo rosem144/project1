133717b2d3dac71e4f1801f991ff772da0ce699e
chrkaw152 <114194285+chrkaw152@users.noreply.github.com>
2023-02-02 16:21:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const rollButton = document.getElementsByTagName("button")[0];
-let result = Math.floor((Math.random()*6)+1);
 rollButton.addEventListener("click", function() {
+  let result = Math.floor((Math.random()*6)+1);
   document.getElementById("dice").innerHTML = result;
 });
 console.log(result);
