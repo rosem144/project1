aae8a8b6efb3a7fdc9fdb2b57d4e69f04c1d468f
chrkaw152 <114194285+chrkaw152@users.noreply.github.com>
2023-02-02 16:45:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,28 @@ rollButton.addEventListener("click", function() {
   //check what the number is and change the image of the dice accordingly
   if(result==1) {
     text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Dice-1-b.svg/557px-Dice-1-b.svg.png)";
+    text.style.height = '120px';
+    text.style.width = '120px';
   } else if(result==2) {
     text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Dice-2a-b.svg/120px-Dice-2a-b.svg.png)";
+    text.style.height = '120px';
+    text.style.width = '120px';
   } else if(result==3){
     text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Dice-3-b.svg/120px-Dice-3-b.svg.png)";
+    text.style.height = '120px';
+    text.style.width = '120px';
   } else if(result==4){
-    text.style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz0E3o3ZLTN4I5nJrwd9YKiHsrnoN9iKvltYBBIl1fVs8c-wu_oug5vPyQE6uXlkG4XAI&usqp=CAU)";
+    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Dice-4-b.svg/1024px-Dice-4-b.svg.png)";
+    text.style.height = '120px';
+    text.style.width = '120px';
   } else if(result==5){
-    text.style.backgroundImage = "url(https://www.pikpng.com/pngl/m/456-4564801_transparent-dice-transparent-six-5-on-a-dice.png)";
+    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Dice-5-b.svg/2048px-Dice-5-b.svg.png)";
+    text.style.height = '120px';
+    text.style.width = '120px';
   } else if(result==6){
     text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Dice-6-b.svg/120px-Dice-6-b.svg.png)";
+    text.style.height = '120px';
+    text.style.width = '120px';
   }
   
   
