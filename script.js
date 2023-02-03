fcd0d582dc6a15d08adb865a21fcf68607bca3a7
chrkaw152 <114194285+chrkaw152@users.noreply.github.com>
2023-02-02 16:56:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ rollButton.addEventListener("click", function() {
   
   //check what the number is and change the image of the dice accordingly
   if(result==1) {
-    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Dice-1-b.svg/557px-Dice-1-b.svg.png)";
+    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/2/2c/Alea_1.png)";
   } else if(result==2) {
-    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Dice-2a-b.svg/120px-Dice-2a-b.svg.png)";
+    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/b/b8/Alea_2.png)";
   } else if(result==3){
-    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Dice-3-b.svg/120px-Dice-3-b.svg.png)";
+    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/2/2f/Alea_3.png)";
   } else if(result==4){
-    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Dice-4-b.svg/1024px-Dice-4-b.svg.png)";
+    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/8/8d/Alea_4.png)";
   } else if(result==5){
-    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Dice-5-b.svg/2048px-Dice-5-b.svg.png)";
+    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/5/55/Alea_5.png)";
   } else if(result==6){
-    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Dice-6-b.svg/120px-Dice-6-b.svg.png)";
+    text.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/f/f4/Alea_6.png)";
   }
   
   //set the text of the dice box to the random number
